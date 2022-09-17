dcfaab9fc51c56dd48a60a873af6b7b5a2fba779
vakharwalad23 <105845275+vakharwalad23@users.noreply.github.com>
2022-09-17 18:33:47 +0530

    Intialize

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Post from './Post/Post.js';
+
+
+const Posts = () =>{
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post />
+            <Post />
+            <Post />
+        </>
+    )
+}
+
+export default Posts;
