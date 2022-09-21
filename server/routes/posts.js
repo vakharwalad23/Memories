96044e5eeb8234ff258b04fc6f3827245fe6043e
vakharwalad23 <105845275+vakharwalad23@users.noreply.github.com>
2022-09-21 18:23:48 +0530

    DELETE: FIXED

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
-router.patch('/:id', deletePost);
+router.delete('/:id', deletePost);
 
 
 export default router;
