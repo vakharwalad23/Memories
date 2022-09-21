640900275e703b9c3b506da5591870522b37c305
vakharwalad23 <105845275+vakharwalad23@users.noreply.github.com>
2022-09-21 19:10:32 +0530

    ALL BUGS SOLVED

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 
-import { deletePost } from '../../../actions/posts.js';
+import { deletePost, likePost } from '../../../actions/posts.js';
 
 import useStyles from './styles.js'
 
@@ -16,14 +16,14 @@ const Post = ( { post, setCurrentId }) =>{
 
     return (
         <Card className={classes.card}>
-            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
+            <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title}/>
             <div className={classes.overlay}>
                 <Typography variant="h6">{post.creator}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
                 <Button style={{color: 'white'}} size="small" onClick={() => setCurrentId(post._id)}>
-                    <MoreHorizIcon fontSize="default" />
+                    <MoreHorizIcon fontSize="medium" />
                 </Button>
             </div>
             <div className={classes.details}>
@@ -34,7 +34,7 @@ const Post = ( { post, setCurrentId }) =>{
                 <Typography variant="h5" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small"/>
                     Like
                     {post.likeCount}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js'
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
 
 // http://localhost:5000/posts
 const router = express.Router();
@@ -10,6 +10,7 @@ router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 
 export default router;
