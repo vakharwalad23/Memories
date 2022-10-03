583700f35e900251a7141b9faa8db9d2b17db4c1
vakharwalad23 <105845275+vakharwalad23@users.noreply.github.com>
2022-10-03 07:27:01 +0530

    Auth Page Design : Done

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
+import LockOutLinedIcon from '@material-ui/icons/LockOutlined';
+import useStyles from './styles';
+import Input from './input';
+
+const Auth = () => {
+    const classes = useStyles();
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const isSignup = true;
+
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    }
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutLinedIcon />
+                </Avatar>
+                <Typography variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSignup && (
+                                <>
+
+                                    <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                                    <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+
+                                </>
+                            )}
+                            <Input name="email" label="Email Address" handleChange={handleChange} type="email"/>
+                            <Input  name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword}/>
+                            { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password"/> }
+                    </Grid>
+                   <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                    {isSignup ? 'Sign Up' : 'Sign In' }
+                    </Button> 
+                </form>
+            </Paper>
+        </Container>
+    )
+}
+
+export default Auth
--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
@@ -14,10 +15,9 @@ app.use(cors());
 
 
 app.use('/posts', postRoutes)
-const CONNECTION_URL = 'mongodb+srv://dhruvjs:dhruvjs@cluster0.moi3sgx.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
